0af8e1baf595f5c74fc4be7e2bbd6d3a3c6e6e7e
Zero-1729 <zero1729@protonmail.com>
2021-09-27 19:31:17 +0100

    misc, ts: fixed type script warning
    It appears to stop the warning the declaration ('*.d.ts') must not be placed in the root of the directory. Additionally, the 'React' import must be within the 'declare block'.

--- types/declarations.d.ts
+++ types/declarations.d.ts
@@ -1,6 +1,5 @@
-import React from 'react';
-
 declare module '*.svg' {
+    import React from 'react';
     import {SvgProps} from 'react-native-svg';
     const content: React.FC<SvgProps>;
     export default content;
