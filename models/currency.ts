a37bc21e5ad7302913b294051028b52fe7689d8d
Zero-1729 <zero1729@protonmail.com>
2021-09-30 07:36:15 +0100

    misc: currency module

---
+++ models/currency.ts
@@ -0,0 +1,232 @@
+export default [
+    {
+        short: 'USD',
+        locale: 'en_US',
+        symbol: '$',
+    },
+    {
+        short: 'AED',
+        locale: 'ar_AE',
+        symbol: 'د.إ.',
+    },
+    {
+        short: 'ARS',
+        locale: 'es_AR',
+        symbol: '$',
+    },
+    {
+        short: 'AUD',
+        locale: 'en_AU',
+        symbol: '$',
+    },
+    {
+        short: 'BDT',
+        locale: 'bn_BD',
+        symbol: '৳',
+    },
+    {
+        short: 'BHD',
+        locale: 'ar_BH',
+        symbol: 'د.ب.',
+    },
+    {
+        short: 'BRL',
+        locale: 'pt_BR',
+        symbol: 'R$',
+    },
+    {
+        short: 'CAD',
+        locale: 'en_CA',
+        symbol: '$',
+    },
+    {
+        short: 'CHF',
+        locale: 'de_CH',
+        symbol: 'CHF',
+    },
+    {
+        short: 'CLP',
+        locale: 'es_CL',
+        symbol: '$',
+    },
+    {
+        short: 'CZK',
+        locale: 'cs',
+        symbol: 'Kč',
+    },
+    {
+        short: 'CNY',
+        locale: 'zh_CN',
+        symbol: '￥',
+    },
+    {
+        short: 'DKK',
+        locale: 'da_DK',
+        symbol: 'kr',
+    },
+    {
+        short: 'EUR',
+        locale: 'de',
+        symbol: '€',
+    },
+    {
+        short: 'GBP',
+        locale: 'en_GB',
+        symbol: '£',
+    },
+    {
+        short: 'HKD',
+        locale: 'zh_HK',
+        symbol: 'HK$',
+    },
+    {
+        short: 'HUF',
+        locale: 'hu_HU',
+        symbol: 'Ft',
+    },
+    {
+        short: 'IDR',
+        locale: 'id',
+        symbol: 'Rp',
+    },
+    {
+        short: 'ILS',
+        locale: 'he',
+        symbol: '₪',
+    },
+    {
+        short: 'INR',
+        locale: 'en_IN',
+        symbol: '₹',
+    },
+    {
+        short: 'JPY',
+        locale: 'ja',
+        symbol: '¥',
+    },
+    {
+        short: 'KRW',
+        locale: 'ko_KR',
+        symbol: '₩',
+    },
+    {
+        short: 'KWD',
+        locale: 'ar_KW',
+        symbol: 'د.ك.',
+    },
+    {
+        short: 'LKR',
+        locale: 'si',
+        symbol: 'Rs',
+    },
+    {
+        short: 'MMK',
+        locale: 'my',
+        symbol: 'K',
+    },
+    {
+        short: 'MXN',
+        locale: 'en_MX',
+        symbol: '$',
+    },
+    {
+        short: 'MYR',
+        locale: 'ms_MY',
+        symbol: 'MR',
+    },
+    {
+        short: 'NGN',
+        locale: 'en_NG',
+        symbol: '₦',
+    },
+    {
+        short: 'NOK',
+        locale: 'nb_NO',
+        symbol: 'kr',
+    },
+    {
+        short: 'NZD',
+        locale: 'en_NZ',
+        symbol: '$',
+    },
+    {
+        short: 'PHP',
+        locale: 'fil',
+        symbol: '₱',
+    },
+    {
+        short: 'PKR',
+        locale: 'ur_PK',
+        symbol: 'ر',
+    },
+    {
+        short: 'PLN',
+        locale: 'pl',
+        symbol: 'zł',
+    },
+    {
+        short: 'RUB',
+        locale: 'ru',
+        symbol: '₽',
+    },
+    {
+        short: 'SAR',
+        locale: 'ar_SA',
+        symbol: 'ر.س.',
+    },
+    {
+        short: 'SEK',
+        locale: 'sv_SE',
+        symbol: 'Kr',
+    },
+    {
+        short: 'SGD',
+        locale: 'en_SG',
+        symbol: '$',
+    },
+    {
+        short: 'THB',
+        locale: 'th',
+        symbol: '฿',
+    },
+    {
+        short: 'TRY',
+        locale: 'tr_TR',
+        symbol: '₺',
+    },
+    {
+        short: 'TWD',
+        locale: 'zh_TW',
+        symbol: '$',
+    },
+    {
+        short: 'UAH',
+        locale: 'uk',
+        symbol: '₴',
+    },
+    {
+        short: 'VEF',
+        locale: 'es_VE',
+        symbol: 'Bs',
+    },
+    {
+        short: 'VND',
+        locale: 'vi',
+        symbol: '₫',
+    },
+    {
+        short: 'ZAR',
+        locale: 'en_ZA',
+        symbol: 'R',
+    },
+    {
+        short: 'XAU',
+        locale: 'xau',
+        symbol: 'XAU',
+    },
+    {
+        short: 'XAG',
+        locale: 'xag',
+        symbol: 'XAG',
+    },
+] as const;
