f38b866da128d438d959e927a9556aff81d6e726
Zero-1729 <zero1729@protonmail.com>
2021-11-23 11:11:11 +0100

    screen, style: added scan icon and bare scan screen

---
+++ screens/Scan.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {StyleSheet} from 'react-native';
+
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const Scan = () => {
+    return <SafeAreaView />;
+};
+
+export default Scan;
+
+const styles = StyleSheet.create({});
