0ed5e8e7a0050ff6d7273fa99313ac490454a858
Zero-1729 <zero1729@protonmail.com>
2022-02-28 03:59:47 +0100

    comp: added new single line input component

--- components/input.tsx
+++ components/input.tsx
@@ -4,6 +4,23 @@ import {TextInput, Platform} from 'react-native';
 
 import tailwind from 'tailwind-rn';
 
+export const TextSingleInput = props => {
+    return (
+        <TextInput
+            underlineColorAndroid="transparent"
+            keyboardType={
+                Platform.OS === 'android' ? 'visible-password' : 'default'
+            }
+            spellCheck={false}
+            autoCorrect={false}
+            autoCapitalize="none"
+            selectTextOnFocus={true}
+            {...props}
+            style={[tailwind('py-4 px-2 rounded text-xs')]}
+        />
+    );
+};
+
 export const TextMultiInput = props => {
     return (
         <TextInput
