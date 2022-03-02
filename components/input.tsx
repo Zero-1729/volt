bdf47c09d266cd62a59746c9b0e270578fb390b3
Zero-1729 <zero1729@protonmail.com>
2022-03-02 03:26:13 +0100

    comp: repackaged input fields as proper comps
    Added callback and extended props support

--- components/input.tsx
+++ components/input.tsx
@@ -1,8 +1,16 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 
-import {TextInput, Platform} from 'react-native';
+import {TextInput, Platform, View} from 'react-native';
 
 import tailwind from 'tailwind-rn';
+
+import DocumentPicker from 'react-native-document-picker';
+
+import {PlainButton} from './button';
+
+import Folder from './../assets/svg/file-directory-fill-24.svg';
+import Scan from './../assets/svg/scan.svg';
 
 export const TextSingleInput = props => {
     return (
@@ -23,21 +31,86 @@ export const TextSingleInput = props => {
 
 export const TextMultiInput = props => {
     return (
-        <TextInput
-            multiline
-            underlineColorAndroid="transparent"
-            keyboardType={
-                Platform.OS === 'android' ? 'visible-password' : 'default'
-            }
-            spellCheck={false}
-            autoCorrect={false}
-            autoCapitalize="none"
-            selectTextOnFocus={false}
-            {...props}
+        <View
             style={[
-                tailwind('py-8 px-4 rounded text-xs h-48'),
-                {textAlignVertical: 'top'},
-            ]}
-        />
+                tailwind('rounded pt-6 pb-10 px-8 h-52'),
+                {
+                    borderWidth: 2,
+                    borderColor: props.borderColor
+                        ? props.borderColor
+                        : 'transparent',
+                },
+            ]}>
+            <TextInput
+                multiline
+                underlineColorAndroid="transparent"
+                keyboardType={
+                    Platform.OS === 'android' ? 'visible-password' : 'default'
+                }
+                spellCheck={false}
+                autoCorrect={false}
+                autoCapitalize="none"
+                selectTextOnFocus={false}
+                {...props}
+                style={[
+                    tailwind('text-xs'),
+                    {
+                        textAlignVertical: 'top',
+                    },
+                ]}
+            />
+
+            {props.showScanIcon ? (
+                <View
+                    style={[
+                        tailwind(
+                            `absolute ${
+                                props.showFolder ? 'right-12' : 'right-4'
+                            } bottom-4`,
+                        ),
+                    ]}>
+                    <PlainButton onPress={() => {}}>
+                        <Scan
+                            width={22}
+                            fill={props.showFolder ? 'gray' : props.folderColor}
+                        />
+                    </PlainButton>
+                </View>
+            ) : (
+                <></>
+            )}
+
+            {props.showFolder ? (
+                <View style={[tailwind('absolute right-4 bottom-4')]}>
+                    <PlainButton
+                        onPress={() => {
+                            DocumentPicker.pickSingle({
+                                type: [
+                                    DocumentPicker.types.allFiles,
+                                    DocumentPicker.types.pdf,
+                                    DocumentPicker.types.plainText,
+                                ],
+                            })
+                                .then(data => {
+                                    props.onSuccess(data);
+                                })
+                                .catch(e => {
+                                    if (DocumentPicker.isCancel(e)) {
+                                        props.onCancel(e);
+                                    } else {
+                                        props.onError(e);
+                                    }
+                                });
+                        }}>
+                        <Folder
+                            width={22}
+                            fill={props.showFolder ? 'gray' : props.folderColor}
+                        />
+                    </PlainButton>
+                </View>
+            ) : (
+                <></>
+            )}
+        </View>
     );
 };
