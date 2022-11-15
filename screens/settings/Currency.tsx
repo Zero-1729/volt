4616dc8cdbe0808cb12d58dbc8c9c532340c0bb3
Zero-1729 <zero1729@protonmail.com>
2022-11-15 01:38:42 +0100

    const: moved currency const

--- screens/settings/Currency.tsx
+++ screens/settings/Currency.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 import {StyleSheet, Text, View, FlatList, useColorScheme} from 'react-native';
 
-import {Currencies, CurrencyType} from '../../models/currency';
+import {Currencies, CurrencyType} from '../../constants/Currency';
 
 import {useNavigation} from '@react-navigation/core';
 
