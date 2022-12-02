9f5f4b5634b92834c36a8a8864ce24394fec8cea
Zero-1729 <zero1729@protonmail.com>
2022-12-02 19:16:39 +0100

    ts: added base prop types (btns, cards, etc.)

---
+++ types/props.d.ts
@@ -0,0 +1,78 @@
+// All reuseable Props and Base Types for RN and App
+import React from 'react';
+import {SvgProps} from 'react-native-svg';
+
+// Base Prop Type
+type BaseProps = {
+    children?: React.ReactNode;
+    style?: React.CSSProperties | StyleProp<ViewStyle>;
+    onPress?: () => void;
+    disabled?: boolean;
+};
+
+type AppCard = BaseProps & {
+    key: React.Key;
+    title: string;
+    description: string;
+    icon: React.FC<SvgProps>;
+    url: string;
+    description: string;
+    color: {
+        backgroundColor: string;
+    };
+    textHue: {
+        color: string;
+    };
+};
+
+// Button prop type
+type ButtonProps = BaseProps & {
+    backgroundColor: string;
+    color?: string;
+    textColor?: string;
+    title: string;
+    props?:
+        | React.ComponentPropsWithoutRef<'button'>
+        | Readonly<TouchableOpacityProps>;
+};
+
+// Base Card Prop Type (for reuse)
+type CardProps = BaseProps & {
+    color?: string;
+    backgroundColor?: string;
+    label: string;
+};
+
+type WalletCardProps = CardProps & {
+    walletBalance: number;
+    walletType: string;
+    isWatchOnly: boolean;
+};
+
+// Base Text Input Prop Type (for reuse)
+type TextInputProps = BaseProps & {
+    color: string;
+    isEnabled?: boolean;
+    placeholder: string;
+    placeholderTextColor?: string;
+    onBlur?: () => void;
+    onChangeText?:
+        | ((text: string) => void)
+        | React.Dispatch<React.SetStateAction<string>>;
+};
+
+// Text Long Input Prop Type
+type TextLongInputProps = BaseProps &
+    TextInputProps & {
+        borderColor?: string;
+        showFolder?: boolean;
+        folderColor?: string;
+        showScanIcon?: boolean;
+        showTestnetToggle?: boolean;
+        onSuccess: (data) => void | boolean;
+        onCancel: (error) => void;
+        onError: (error) => void;
+        toggleSwitch?:
+            | (() => void)
+            | React.Dispatch<React.SetStateAction<boolean>>;
+    };
