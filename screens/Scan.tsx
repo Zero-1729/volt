624459f0e8ac5a7ed26d97f02ba64189adc4189e
Zero-1729 <zero1729@protonmail.com>
2022-12-31 00:49:06 +0100

    screen: Added rough loading logic in scan
    TODO: clean up logic

--- screens/Scan.tsx
+++ screens/Scan.tsx
@@ -49,9 +49,9 @@ const LoadingView = (props: any) => {
                     tailwind('text-sm text-center'),
                 ]}>
                 {/* Only show loading if actually loading */}
-                {props.camera === undefined
+                {props.isCamAvailable === false
                     ? 'Camera is not available'
-                    : 'Loading'}
+                    : 'Loading...'}
             </Text>
         </SafeAreaView>
     );
@@ -96,13 +96,13 @@ const Scan = () => {
     const isFocused = useIsFocused();
     const navigation = useNavigation();
 
-    const [isLoading, setIsLoading] = useState(false);
+    // Assume Camera loading until we know otherwise
+    // If unavailable, we'll show a message
+    // Else, we'll cut it out and let Camera View take over
+    const [isLoading, setIsLoading] = useState(true);
     const [grantedPermission, setGrantedPermission] =
         useState<CameraPermissionStatus>('not-determined');
-
-    const closeScreen = () => {
-        navigation.dispatch(CommonActions.goBack());
-    };
+    const [isCamAvailable, setCamAvailable] = useState<boolean | null>(null);
 
     const onQRDetected = useCallback(
         async (data: Barcode[]) => {
@@ -124,6 +124,10 @@ const Scan = () => {
         [navigation],
     );
 
+    const closeScreen = () => {
+        navigation.dispatch(CommonActions.goBack());
+    };
+
     // Update permission state when permission changes.
     const updatePermissions = useCallback(async () => {
         const status = await Camera.requestCameraPermission();
@@ -131,10 +135,29 @@ const Scan = () => {
         setGrantedPermission(status);
     }, []);
 
+    const updateCameraAvail = useCallback(async () => {
+        // Get available camera devices
+        const device = await Camera.getAvailableCameraDevices();
+
+        // If array empty, we assume no camera is available
+        setCamAvailable(device.length > 0);
+    }, []);
+
     // Note, IOS simulator does not support the camera,
     // so you need a physical device to test.
     const devices = useCameraDevices();
     const camera = devices.back;
+
+    useEffect(() => {
+        // Update camera availability if not determined.
+        updateCameraAvail();
+
+        // If camera available initialized
+        // cut out of loading
+        if (isCamAvailable !== null) {
+            setIsLoading(false);
+        }
+    }, [isCamAvailable, updateCameraAvail]);
 
     useEffect(() => {
         // Update permission setting if not determined.
@@ -166,7 +189,7 @@ const Scan = () => {
 
     // Display loading or camera unavailable; handle differently
     if (isLoading || camera === undefined) {
-        return <LoadingView camera={camera} />;
+        return <LoadingView isCamAvailable={isCamAvailable} />;
     }
 
     // Display Camera view if camera available
