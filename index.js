c71e8a963d8ec117229b5bed83cad7020abf4a4d
Zero-1729 <zero1729@protonmail.com>
2022-12-30 07:11:23 +0100

    app: included RN-reanimated in index for FP

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
  * @format
  */
 
+// For frameProcessor in 'react-native-vision-camera'
+import 'react-native-reanimated';
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
