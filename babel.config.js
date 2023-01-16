5d5e1f6467189aaf2f448a4fc107775371e4af46
Zero-1729 <zero1729@protonmail.com>
2023-01-16 12:35:11 +0100

    dep: updated babel aliases for crypto

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,16 @@ module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [
         [
+            'module-resolver',
+            {
+                alias: {
+                    crypto: 'react-native-quick-crypto',
+                    stream: 'stream-browserify',
+                    buffer: '@craftzdog/react-native-buffer',
+                },
+            },
+        ],
+        [
             'react-native-reanimated/plugin',
             {
                 globals: ['__scanCodes'],
