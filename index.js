0bbe75bedd30d864cd7415923147918600f2e874
Zero-1729 <zero1729@protonmail.com>
2023-02-02 05:30:42 +0100

    app: added app context and store provider

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-/**
- * @format
- */
+import React from 'react';
+import {AppRegistry} from 'react-native';
 
 // For frameProcessor in 'react-native-vision-camera'
 import 'react-native-reanimated';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import {AppStorageProvider} from './class/storageContext';
 
-AppRegistry.registerComponent(appName, () => App);
+import App from './App';
+
+const AppWithStorage = () => {
+    return (
+        <AppStorageProvider>
+            <App />
+        </AppStorageProvider>
+    );
+};
+
+AppRegistry.registerComponent(appName, () => AppWithStorage);
