cf72d307e2b5eed4c3b6c6798381bf2dc8353741
Zero-1729 <zero1729@protonmail.com>
2023-02-04 03:08:56 +0100

    class: matched BaseWallet names with Base Type

--- class/wallet/base.ts
+++ class/wallet/base.ts
@@ -13,34 +13,34 @@ const Units = {
 };
 
 export class BaseWallet {
-    _ID: string;
+    id: string;
 
     name: string;
     isWatchOnly: boolean;
     type: string;
 
-    _descriptor: string;
+    descriptor: string;
     birthday: string | Date;
-    _secret: string;
+    secret: string;
 
-    _MasterFingerprint: string;
+    MasterFingerprint: string;
     isBIP39: boolean;
 
     balance: number;
 
     UTXOs: UTXOType[];
 
-    _addresses: Array<string>;
-    _address: string;
+    addresses: Array<string>;
+    address: string;
 
-    _syncedBalance: number;
-    _lastSynced: number;
-    _units: Unit;
+    syncedBalance: number;
+    lastSynced: number;
+    units: Unit;
 
     network: string;
 
-    _hardwareWalletEnabled: boolean;
-    _hasBackedUp: boolean;
+    hardwareWalletEnabled: boolean;
+    hasBackedUp: boolean;
 
     constructor(
         name: string,
@@ -50,35 +50,35 @@ export class BaseWallet {
         descriptor?: string,
         network?: string,
     ) {
-        this._ID = this._generateID(); // Unique wallet ID
+        this.id = this._generateID(); // Unique wallet ID
         this.name = name; // Wallet name
 
         this.isWatchOnly = isWatchOnly; // Whether wallet is watch only
-        this.type = type; // Can have 'segwit', 'legacy', or 'bech32' wallets
+        this.type = WalletTypes[type]; // Can have 'segwit native', 'segwit', 'legacy', etc. wallets
 
-        this._addresses = []; // List of addresses
-        this._address = ''; // Temporarily generated receiving address
-        this._descriptor = descriptor ? descriptor : '';
+        this.addresses = []; // List of addresses
+        this.address = ''; // Temporarily generated receiving address
+        this.descriptor = descriptor ? descriptor : '';
         this.birthday = Date(); // Timestamp of wallet creation
 
-        this._units = Units.SATS; // Which unit to display wallet balance in
+        this.units = Units.SATS; // Which unit to display wallet balance in
 
         this.balance = 0; // By default the balance is in sats
-        this._syncedBalance = 0; // Last balance synced from node
-        this._lastSynced = 0; // Timestamp of last wallet sync
+        this.syncedBalance = 0; // Last balance synced from node
+        this.lastSynced = 0; // Timestamp of last wallet sync
         this.network = network ? network : 'mainnet'; // Can have 'mainnet', 'testnet', or 'signet' wallets
 
         this.UTXOs = []; // Set of wallet transactions
 
-        this._hardwareWalletEnabled = false;
-        this._hasBackedUp = false; // Whether user has backed up seed
+        this.hardwareWalletEnabled = false;
+        this.hasBackedUp = false; // Whether user has backed up seed
 
-        this._MasterFingerprint = ''; // Wallet master fingerprint
-        this._secret = secret; // private key or recovery phrase
+        this.MasterFingerprint = ''; // Wallet master fingerprint
+        this.secret = secret; // private key or recovery phrase
         this.isBIP39 = false; // Whether wallet has a 'BIP39' seed
     }
 
-    _generateID() {
+    private _generateID() {
         return Crypto.randomUUID();
     }
 }
