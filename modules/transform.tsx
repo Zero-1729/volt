2b8b625d40258e31ee6f7eb048b52d7fadb5bb74
Zero-1729 <zero1729@protonmail.com>
2023-02-06 19:14:35 +0100

    mod: addCommas now takes in string and returns comma or space

--- modules/transform.tsx
+++ modules/transform.tsx
@@ -1,3 +1,3 @@
-export const addCommas = (num: number) => {
-    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+export const addCommas = (num: string, separator: string = ',') => {
+    return num.replace(/\B(?=(\d{3})+(?!\d))/g, separator);
 };
