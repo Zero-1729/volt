e864b88060109d52fdb23c02250b6507fd21b7e3
Zero-1729 <zero1729@protonmail.com>
2023-02-14 00:10:43 +0100

    misc: took out unused stylesheet decl

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 
-import {StatusBar, StyleSheet, useColorScheme} from 'react-native';
+import {StatusBar, useColorScheme} from 'react-native';
 
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
