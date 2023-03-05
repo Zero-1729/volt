4797a30541b7ac8602b6ac454914f0d61a442239
Zero-1729 <zero1729@protonmail.com>
2023-03-05 20:53:51 +0100

    screen: added basic template for wallet view

--- screens/wallet/Wallet.tsx
+++ screens/wallet/Wallet.tsx
@@ -1,10 +1,35 @@
-import {View} from 'react-native';
 import React from 'react';
+import {useColorScheme, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useTailwind} from 'tailwind-rn';
+
+import Color from '../../constants/Color';
 
 const Wallet = () => {
+    const tailwind = useTailwind();
+    const ColorScheme = Color(useColorScheme());
+
+    // Get card color from wallet type
+    const CardColor = ColorScheme.WalletColors.bech32;
+
     // Receive Wallet ID and fetch wallet data to display
     // Include functions to change individual wallet settings
-    return <View />;
+    return (
+        <SafeAreaView>
+            <View style={[tailwind('w-full h-full')]}>
+                {/* Top panel */}
+                <View
+                    style={[tailwind('relative'), {backgroundColor: CardColor}]}
+                />
+
+                {/* Send and receive */}
+                <View style={[tailwind('w-full h-48 justify-center')]} />
+
+                {/* Transactions List */}
+                <View />
+            </View>
+        </SafeAreaView>
+    );
 };
 
 export default Wallet;
