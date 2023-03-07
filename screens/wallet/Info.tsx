5fde5cd17f9a0fe8c2abced6ccfdc08cd2138e67
Zero-1729 <zero1729@protonmail.com>
2023-03-07 06:51:27 +0100

    screen: add wallet screen info

---
+++ screens/wallet/Info.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const Info = () => {
+    return (
+        <SafeAreaView>
+            {/* Display Wallet Info, addresses, and other related data / settings */}
+            <View>
+                <Text>Wallet Info</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default Info;
