c3401cc74dfd372a676316bd9bca8055a08972ab
Zero-1729 <zero1729@protonmail.com>
2023-03-08 07:34:33 +0100

    screen: fleshed out wallet info some more

--- screens/wallet/Info.tsx
+++ screens/wallet/Info.tsx
@@ -1,13 +1,57 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, useColorScheme} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {useNavigation, CommonActions} from '@react-navigation/native';
+
+import {PlainButton, LongBottomButton} from '../../components/button';
+
+import {useTailwind} from 'tailwind-rn';
+import Color from '../../constants/Color';
+
+import Back from '../../assets/svg/arrow-left-24.svg';
 
 const Info = () => {
+    const tailwind = useTailwind();
+    const ColorScheme = Color(useColorScheme());
+    const navigation = useNavigation();
+
     return (
-        <SafeAreaView>
+        <SafeAreaView edges={['bottom', 'right', 'left']}>
             {/* Display Wallet Info, addresses, and other related data / settings */}
-            <View>
-                <Text>Wallet Info</Text>
+            <View style={[tailwind('w-full h-full items-center relative')]}>
+                <PlainButton
+                    style={[tailwind('items-center mt-6 flex-row w-5/6')]}
+                    onPress={() => {
+                        navigation.dispatch(CommonActions.goBack());
+                    }}>
+                    <Back
+                        style={tailwind('mr-2')}
+                        fill={ColorScheme.SVG.Default}
+                    />
+                    {/* Wallet name */}
+                    <Text
+                        style={[
+                            tailwind('text-white font-bold'),
+                            {color: ColorScheme.Text.Default},
+                        ]}>
+                        Wallet Name
+                    </Text>
+                </PlainButton>
+
+                {/* Wallet Info */}
+                {/* Wallet Addresses */}
+                {/* Wallet Tools */}
+
+                {/* Delete Wallet btn */}
+                <LongBottomButton
+                    title="Delete"
+                    onPress={() => {
+                        console.info('[Action] Delete Wallet');
+                    }}
+                    textColor={'red'}
+                    backgroundColor={ColorScheme.Background.Greyed}
+                    style={[tailwind('font-bold')]}
+                />
             </View>
         </SafeAreaView>
     );
