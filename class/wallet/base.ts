829a5e8dd513c7def4e4ad563a2fdb49fce17188
Zero-1729 <zero1729@protonmail.com>
2023-03-09 15:10:14 +0100

    class: assume BIP39 if mnemonic detected
    Will be words with spaces (i.e., 'dog alice', etc.)

--- class/wallet/base.ts
+++ class/wallet/base.ts
@@ -85,7 +85,7 @@ export class BaseWallet {
         // TODO: fetch from BDK
         this.masterFingerprint = ''; // Wallet master fingerprint
         this.secret = secret ? secret : generateMnemonic(); // private key or recovery phrase
-        this.isBIP39 = false; // Whether wallet has a 'BIP39' seed
+        this.isBIP39 = this.secret.includes(' ') ? true : false; // Whether wallet has a 'BIP39' seed
 
         this.isWatchOnly = !this.secret; // Whether wallet is watch only
     }
