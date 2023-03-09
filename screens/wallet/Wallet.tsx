2099aad9559d38bd6a3b530eddce5d8ff6acb0ce
Zero-1729 <zero1729@protonmail.com>
2023-03-09 08:47:29 +0100

    screen: updated wallet screen UI
    Included transaction view (cond, if any else default info message)

--- screens/wallet/Wallet.tsx
+++ screens/wallet/Wallet.tsx
@@ -10,6 +10,7 @@ import Color from '../../constants/Color';
 import Dots from '../../assets/svg/kebab-horizontal-24.svg';
 import Scan from '../../assets/svg/scan.svg';
 import Back from '../../assets/svg/arrow-left-24.svg';
+import Box from '../../assets/svg/inbox-24.svg';
 
 import {PlainButton} from '../../components/button';
 
@@ -21,6 +22,9 @@ const Wallet = () => {
     // Get card color from wallet type
     const CardColor = ColorScheme.WalletColors.bech32;
 
+    // TODO: Fetch transactions from wallet
+    const transactions = [];
+
     // Receive Wallet ID and fetch wallet data to display
     // Include functions to change individual wallet settings
     return (
@@ -29,7 +33,7 @@ const Wallet = () => {
                 {/* Top panel */}
                 <View
                     style={[
-                        tailwind('relative h-48'),
+                        tailwind('relative h-1/3 rounded-b-2xl'),
                         {backgroundColor: CardColor},
                     ]}>
                     <View
@@ -61,61 +65,132 @@ const Wallet = () => {
                         </PlainButton>
                     </View>
 
-                    <View style={[tailwind('absolute left-6 bottom-6')]}>
-                        <Text style={[tailwind('text-sm text-white mb-2')]}>
-                            Wallet Name
+                    {/* Balance */}
+                    <View
+                        style={[
+                            tailwind('absolute self-center w-5/6 bottom-28'),
+                        ]}>
+                        <Text
+                            style={[tailwind('text-sm text-white opacity-60')]}>
+                            Balance
                         </Text>
-                        <Text style={[tailwind('text-2xl text-white')]}>
+                        <Text style={[tailwind('text-3xl text-white')]}>
                             0 Sats
                         </Text>
                     </View>
-                </View>
 
-                {/* Send and receive */}
-                <View style={[tailwind('justify-evenly flex-row mt-4 mb-4')]}>
+                    {/* Send and receive */}
                     <View
                         style={[
-                            tailwind('rounded p-4 w-32'),
-                            {backgroundColor: ColorScheme.Background.Greyed},
+                            tailwind(
+                                'absolute bottom-4 w-full justify-evenly flex-row mt-4 mb-4',
+                            ),
                         ]}>
-                        <PlainButton>
-                            <Text
-                                style={[
-                                    tailwind('text-sm text-center font-bold'),
-                                    {color: ColorScheme.Text.Default},
-                                ]}>
-                                Send
-                            </Text>
-                        </PlainButton>
+                        <View
+                            style={[
+                                tailwind('rounded p-4 w-32'),
+                                {
+                                    backgroundColor:
+                                        ColorScheme.Background.Primary,
+                                },
+                            ]}>
+                            <PlainButton>
+                                <Text
+                                    style={[
+                                        tailwind(
+                                            'text-sm text-center font-bold',
+                                        ),
+                                        {color: ColorScheme.Text.Default},
+                                    ]}>
+                                    Send
+                                </Text>
+                            </PlainButton>
+                        </View>
+                        <View
+                            style={[
+                                tailwind('rounded p-4 w-32'),
+                                {
+                                    backgroundColor:
+                                        ColorScheme.Background.Primary,
+                                },
+                            ]}>
+                            <PlainButton>
+                                <Text
+                                    style={[
+                                        tailwind(
+                                            'text-sm text-center font-bold',
+                                        ),
+                                        {color: ColorScheme.Text.Default},
+                                    ]}>
+                                    Receive
+                                </Text>
+                            </PlainButton>
+                        </View>
+                        <View
+                            style={[
+                                tailwind('justify-center rounded px-4'),
+                                {
+                                    backgroundColor:
+                                        ColorScheme.Background.Primary,
+                                },
+                            ]}>
+                            <PlainButton>
+                                <Scan
+                                    width={32}
+                                    fill={ColorScheme.SVG.Default}
+                                />
+                            </PlainButton>
+                        </View>
                     </View>
+
+                    {/* Bottom line divider */}
                     <View
                         style={[
-                            tailwind('rounded p-4 w-32'),
-                            {backgroundColor: ColorScheme.Background.Greyed},
-                        ]}>
-                        <PlainButton>
-                            <Text
-                                style={[
-                                    tailwind('text-sm text-center font-bold'),
-                                    {color: ColorScheme.Text.Default},
-                                ]}>
-                                Receive
-                            </Text>
-                        </PlainButton>
-                    </View>
-                    <View
-                        style={[
-                            tailwind('justify-center rounded px-4'),
-                            {backgroundColor: ColorScheme.Background.Greyed},
-                        ]}>
-                        <PlainButton>
-                            <Scan width={32} fill={ColorScheme.SVG.Default} />
-                        </PlainButton>
-                    </View>
+                            tailwind(
+                                'w-16 h-1 absolute bottom-2 rounded-full mt-2 self-center',
+                            ),
+                            {backgroundColor: ColorScheme.Background.Primary},
+                        ]}
+                    />
                 </View>
 
                 {/* Transactions List */}
-                <View />
+                <View style={[tailwind('h-2/3 w-full')]}>
+                    <View style={[tailwind('ml-6 mt-6')]}>
+                        <Text
+                            style={[
+                                tailwind('text-lg font-bold'),
+                                {color: ColorScheme.Text.Default},
+                            ]}>
+                            Transactions
+                        </Text>
+                    </View>
+
+                    {transactions.length === 0 ? (
+                        <View
+                            style={[
+                                tailwind(
+                                    'flex mt-6 justify-around text-justify h-5/6 items-center justify-center',
+                                ),
+                            ]}>
+                            <Box
+                                width={32}
+                                fill={ColorScheme.SVG.GrayFill}
+                                style={tailwind('mb-4 -mt-6')}
+                            />
+                            <Text
+                                style={[
+                                    tailwind('w-3/5 text-center'),
+                                    {color: ColorScheme.Text.GrayedText},
+                                ]}>
+                                A list of all transactions for this wallet be
+                                displayed here
+                            </Text>
+                        </View>
+                    ) : (
+                        <View />
+                    )}
+                </View>
             </View>
         </SafeAreaView>
     );
