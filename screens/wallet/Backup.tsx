7412d9b903e0396a8651eec72f501bd6b3aa043c
Zero-1729 <zero1729@protonmail.com>
2023-03-09 21:09:18 +0100

    screen, style: can toggle between mnemonic and descriptor in wallet info

--- screens/wallet/Backup.tsx
+++ screens/wallet/Backup.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 
 import {Text, View, useColorScheme} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
@@ -26,6 +26,8 @@ const Backup = () => {
     const tailwind = useTailwind();
     const ColorScheme = Color(useColorScheme());
 
+    const [backupMaterial, setBackupMaterial] = useState<string>('mnemonic');
+
     const {currentWalletID, getWalletData, isAdvancedMode} =
         useContext(AppStorageContext);
 
@@ -37,9 +39,13 @@ const Backup = () => {
         (isAdvancedMode ? ` (${WalletTypeNames[walletData.type][1]})` : '');
 
     const warning =
-        'This seed is the only way to recover your wallet. If you lose it, you will lose your funds. Please write it down and keep it in a safe place. Do not Screenshot or share it with anyone.';
+        'This material is the only way to recover your wallet. If you lose it, you will lose your funds. Please write it down and keep it in a safe place. Do not Screenshot or share it with anyone.';
 
-    const mnemonicData = walletData.secret;
+    const getMnemonicData = () => {
+        return backupMaterial === 'mnemonic'
+            ? walletData.secret
+            : walletData.descriptor;
+    };
 
     return (
         <SafeAreaView>
@@ -95,32 +101,99 @@ const Backup = () => {
                         {walletTypeName}
                     </Text>
 
-                    {/* Display QR code with seed */}
-                    <View style={[tailwind('self-center mb-8')]}>
-                        <QRCode value={mnemonicData} size={256} />
+                    {/* Display wallet seed */}
+                    <View
+                        style={[
+                            tailwind(
+                                'flex-row self-center items-center justify-center rounded-sm p-2 px-4 mb-4 bg-blue-800',
+                            ),
+                            {backgroundColor: ColorScheme.Background.Greyed},
+                        ]}>
+                        <PlainButton
+                            style={[tailwind('mr-4')]}
+                            disabled={backupMaterial === 'mnemonic'}
+                            onPress={() => {
+                                setBackupMaterial('mnemonic');
+                            }}>
+                            <Text
+                                style={[
+                                    tailwind(
+                                        `text-base ${
+                                            backupMaterial === 'mnemonic'
+                                                ? 'font-bold'
+                                                : ''
+                                        }`,
+                                    ),
+                                    {
+                                        color:
+                                            backupMaterial === 'mnemonic'
+                                                ? ColorScheme.Text.Default
+                                                : ColorScheme.Text.GrayText,
+                                    },
+                                ]}>
+                                Mnemonic
+                            </Text>
+                        </PlainButton>
+                        <View
+                            style={[
+                                tailwind('h-6 w-0.5 mr-4 rounded-full'),
+                                {
+                                    backgroundColor:
+                                        ColorScheme.Background.CardGreyed,
+                                },
+                            ]}
+                        />
+                        <PlainButton
+                            disabled={backupMaterial === 'descriptor'}
+                            onPress={() => {
+                                setBackupMaterial('descriptor');
+                            }}>
+                            <Text
+                                style={[
+                                    tailwind(
+                                        `text-base ${
+                                            backupMaterial === 'descriptor'
+                                                ? 'font-bold'
+                                                : ''
+                                        }`,
+                                    ),
+                                    {
+                                        color:
+                                            backupMaterial === 'descriptor'
+                                                ? ColorScheme.Text.Default
+                                                : ColorScheme.Text.GrayedText,
+                                    },
+                                ]}>
+                                Descriptor
+                            </Text>
+                        </PlainButton>
                     </View>
 
-                    {/* Display wallet seed */}
+                    {/* Display QR code with seed */}
+                    <View style={[tailwind('self-center mb-4')]}>
+                        <QRCode value={getMnemonicData()} size={256} />
+                    </View>
+
+                    {/* Display either seed or descriptor */}
                     <PlainButton style={[tailwind('items-center')]}>
                         <Text
                             style={[
-                                tailwind('flex-row p-3 rounded-sm'),
+                                tailwind(
+                                    'text-base p-3 text-center rounded-sm',
+                                ),
                                 {
                                     backgroundColor:
                                         ColorScheme.Background.Greyed,
                                     color: ColorScheme.Text.Default,
                                 },
-                            ]}>
-                            <Text
-                                style={[tailwind('text-base font-bold mr-2')]}>
-                                Seed:{' '}
-                            </Text>
-                            <Text
-                                style={[tailwind('text-base')]}
-                                numberOfLines={2}
-                                lineBreakMode={'clip'}>
-                                {walletData.secret}
-                            </Text>
+                            ]}
+                            numberOfLines={
+                                backupMaterial === 'mnemonic' ? 2 : 1
+                            }
+                            ellipsizeMode={'middle'}>
+                            {backupMaterial === 'mnemonic'
+                                ? walletData.secret
+                                : walletData.descriptor}
                         </Text>
                     </PlainButton>
 
