4ee7e189316fcaff5e679d555033d5b45fcedb27
Zero-1729 <zero1729@protonmail.com>
2023-03-12 15:27:44 +0100

    comp: removed unused style in input component

--- components/input.tsx
+++ components/input.tsx
@@ -30,9 +30,7 @@ export const TextSingleInput = (props: TextInputProps) => {
             {...props}
             style={[
                 tailwind(
-                    `${
-                        props.shavedHeight ? 'py-2' : 'py-4'
-                    } px-2 rounded text-xs`,
+                    `${props.shavedHeight ? 'py-2' : 'py-4'} px-2 text-xs`,
                 ),
             ]}
         />
