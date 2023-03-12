84f11a8f6fd24297c6a57300742cce06bd4036e0
Zero-1729 <zero1729@protonmail.com>
2023-03-12 16:35:28 +0100

    comp: added alert for deletion

--- components/alert.tsx
+++ components/alert.tsx
@@ -3,3 +3,23 @@ import {Alert} from 'react-native';
 export const alert = (heading: string, msg: string): void => {
     Alert.alert(heading, msg);
 };
+
+export const DeletionAlert = (
+    heading: string,
+    msg: string,
+    primaryButtonText: string,
+    onSuccess: () => void,
+): void => {
+    Alert.alert(heading, msg, [
+        {
+            text: 'Cancel',
+            onPress: () => {},
+            style: 'cancel',
+        },
+        {
+            text: primaryButtonText,
+            onPress: onSuccess,
+            style: 'destructive',
+        },
+    ]);
+};
