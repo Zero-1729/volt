c99277e3578d77a32254612df9bbeeb3da2b1a73
Zero-1729 <zero1729@protonmail.com>
2023-05-13 22:58:46 +0100

    mod: updated balance fns with new BalanceType

--- modules/transform.tsx
+++ modules/transform.tsx
@@ -1,3 +1,5 @@
+import {BalanceType} from '../types/wallet';
+
 const SATS_TO_BTC_RATE = 100_000_000;
 const SEPARATOR = ' ';
 
@@ -5,17 +7,17 @@ export const addCommas = (num: string, separator: string = ',') => {
     return num.replace(/\B(?=(\d{3})+(?!\d))/g, separator);
 };
 
-export const _getBTCfromSats = (sats: number) => {
-    return sats / SATS_TO_BTC_RATE;
+export const _getBTCfromSats = (sats: BalanceType) => {
+    return sats.toNumber() / SATS_TO_BTC_RATE;
 };
 
-export const formatSats = (sats: number) => {
+export const formatSats = (sats: BalanceType) => {
     // REM: Based on: https://bitcoin.design/guide/designing-products/units-and-symbols/
-    if (sats < 0.1 && sats !== 0) {
+    if (sats.lt(0.1) && !sats.eq(0)) {
         // Limit display to eight decimals
         // Anything lower is assumed to be an approximation
         // ... to zero
-        if (sats < 0.00_000_001) {
+        if (sats.lt(0.00_000_001)) {
             return `≈ ${sats.toFixed(2)}`;
         }
 
@@ -25,15 +27,15 @@ export const formatSats = (sats: number) => {
 
     // If billions range of sats
     // We display with units
-    if (sats > SATS_TO_BTC_RATE) {
-        return formatWithUnits(sats);
+    if (sats.gt(SATS_TO_BTC_RATE)) {
+        return formatWithUnits(sats.toNumber());
     }
 
     // Strip trailing zeros
     return addCommas(sats.toString(), SEPARATOR);
 };
 
-export const formatBTC = (sats: number) => {
+export const formatBTC = (sats: BalanceType) => {
     // REM: Based on: https://bitcoin.design/guide/designing-products/units-and-symbols/
     const BTC = _getBTCfromSats(sats);
 
@@ -76,9 +78,9 @@ const formatWithUnits = (value: number) => {
     return value.toFixed(2);
 };
 
-export const normalizeFiat = (sats: number, rate: number) => {
+export const normalizeFiat = (sats: BalanceType, rate: number) => {
     // Get BTC to fiat value first
-    const fiat = _getBTCfromSats(sats) * rate;
+    const fiat = sats.eq(0) ? 0 : _getBTCfromSats(sats) * rate;
 
     // If below a cent, let's attempt to display that
     // 'Bullishly' speaking, Bitcoin will 'always' be worth more than a cent
