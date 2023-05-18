f12e7cd8ebbb77210859b88cdea46a520b02497a
Zero-1729 <zero1729@protonmail.com>
2023-05-18 02:09:12 +0100

    mod: added mod for mempool fns

---
+++ modules/mempool.ts
@@ -0,0 +1,39 @@
+import mempoolJS from '@mempool/mempool.js';
+
+export const getTxData = async (txid: string, network: string) => {
+    try {
+        const {
+            bitcoin: {transactions},
+        } = mempoolJS({
+            hostname: 'mempool.space',
+            network: network,
+        });
+
+        // Get transaction data
+        const tx = await transactions.getTx({txid});
+
+        // Return transaction data
+        return tx;
+    } catch (e) {
+        throw e;
+    }
+};
+
+export const getAddressInfo = async (address: string, network: string) => {
+    try {
+        const {
+            bitcoin: {addresses},
+        } = mempoolJS({
+            hostname: 'mempool.space',
+            network: network,
+        });
+
+        // Get address data
+        const data = await addresses.getAddress({address});
+
+        // Return address data
+        return data;
+    } catch (e) {
+        throw e;
+    }
+};
