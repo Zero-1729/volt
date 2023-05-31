d2b57e0dc10a9f61f826cda9a23792a7e7393abe
Zero-1729 <zero1729@protonmail.com>
2023-05-31 05:45:31 +0100

    class: added independent wallet classes

---
+++ class/wallet/segwit/bech32.ts
@@ -0,0 +1,52 @@
+import {BaseWallet} from './../base';
+import {
+    getAddressPath,
+    generateAddressFromPath,
+} from './../../../modules/wallet-utils';
+
+import {TransactionType, addressType} from './../../../types/wallet';
+
+export class SegWitNativeWallet extends BaseWallet {
+    generateNewAddress(): addressType {
+        try {
+            const addressPath = getAddressPath(
+                this.index,
+                false,
+                this.network,
+                this.type,
+            );
+
+            const address = generateAddressFromPath(
+                addressPath,
+                this.network,
+                this.type,
+                this.secret,
+            );
+
+            // Bump address index
+            this.index++;
+
+            return {
+                address: address,
+                path: this.derivationPath,
+                change: false,
+                index: this.index,
+                memo: '',
+            };
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    buildTx(args: any) {
+        throw new Error('Not implemented');
+    }
+
+    updatedTransaction(tx: TransactionType) {
+        throw new Error('Not implemented');
+    }
+
+    updateTransanctions(transactions: TransactionType[]) {
+        this.transactions = transactions;
+    }
+}
