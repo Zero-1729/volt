84ef2aa715bcacdbf41df80ae97a45a921aa0254
Zero-1729 <zero1729@protonmail.com>
2023-06-01 23:05:25 +0100

    comp: added template number pad

---
+++ components/numberpad.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const Numberpad = () => {
+    return (
+        <View>
+            <Text>numberpad;</Text>
+        </View>
+    );
+};
+
+export default Numberpad;
