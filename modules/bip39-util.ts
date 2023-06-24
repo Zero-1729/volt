216b1cb7142f3e767f614d0229e42e5179f9250a
Zero-1729 <zero1729@protonmail.com>
2023-06-24 20:01:49 +0100

    misc: minor code reorg

--- modules/bip39-util.ts
+++ modules/bip39-util.ts
@@ -7,17 +7,6 @@ const bip32 = BIP32Factory(ecc);
 import {NetType} from '../types/wallet';
 
 export const BJSNetworks: {[index: string]: any} = {
-    bitcoin: {
-        messagePrefix: '\x18Bitcoin Signed Message:\n',
-        bech32: 'bc',
-        bip32: {
-            public: 0x0488b21e,
-            private: 0x0488ade4,
-        },
-        pubKeyHash: 0x00,
-        scriptHash: 0x05,
-        wif: 0x80,
-    },
     testnet: {
         messagePrefix: '\x18Bitcoin Signed Message:\n',
         bech32: 'tb',
@@ -28,6 +17,17 @@ export const BJSNetworks: {[index: string]: any} = {
         pubKeyHash: 0x6f,
         scriptHash: 0xc4,
         wif: 0xef,
+    },
+    bitcoin: {
+        messagePrefix: '\x18Bitcoin Signed Message:\n',
+        bech32: 'bc',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4,
+        },
+        pubKeyHash: 0x00,
+        scriptHash: 0x05,
+        wif: 0x80,
     },
 };
 
