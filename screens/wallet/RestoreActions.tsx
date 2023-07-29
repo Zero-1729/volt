a81a3041b01dfd5aa75807e588d140192151b5b2
Zero-1729 <zero1729@protonmail.com>
2023-07-29 07:49:38 +0100

    misc: reverted text based on descriptor text in core

--- screens/wallet/RestoreActions.tsx
+++ screens/wallet/RestoreActions.tsx
@@ -132,7 +132,7 @@ const ImportAction = () => {
             if (!isDescriptorPattern(descriptor)) {
                 errorAlert(
                     'Descriptor',
-                    'Only single-sig descriptors are supported (i.e. wpkh([...]...), pkh([...]...), sh(wpkh([...]...))',
+                    'Only single-key descriptors are supported (i.e. wpkh([...]...), pkh([...]...), sh(wpkh([...]...))',
                 );
 
                 return;
