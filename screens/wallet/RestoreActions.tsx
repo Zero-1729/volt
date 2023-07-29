5f56f46712aa248e0d9c4193ec218a9a1699cb8f
Zero-1729 <zero1729@protonmail.com>
2023-07-29 19:21:57 +0100

    screen: set xkey network if avail to import fn

--- screens/wallet/RestoreActions.tsx
+++ screens/wallet/RestoreActions.tsx
@@ -156,7 +156,7 @@ const ImportAction = () => {
             await restoreWallet(
                 extendedKey,
                 getExtendedKeyPrefix(extendedKey),
-                network,
+                getNetworkIfXkey(extendedKey),
             );
 
             // Clear input
