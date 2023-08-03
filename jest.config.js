b9efa149fb553f10c09f4e67f05f76f996f46919
Zero-1729 <zero1729@protonmail.com>
2023-08-03 06:20:08 +0100

    dep: consolidate and update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
 module.exports = {
     preset: 'react-native',
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transformIgnorePatterns: [
+        'node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?)/)',
+    ],
+    watchPathIgnorePatterns: ['<rootDir>/node_modules'],
+    verbose: true,
 };
