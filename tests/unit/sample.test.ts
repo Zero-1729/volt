d281c695c90017c1f2357762a1f37a6b8aaecb54
Zero-1729 <zero1729@protonmail.com>
2023-08-03 20:40:42 +0100

    test: took out sample test

--- tests/unit/sample.test.ts
+++
@@ -1,7 +0,0 @@
-import {describe, expect, test} from '@jest/globals';
-
-describe('testing index file', () => {
-    test('Two minus two should result in zero', () => {
-        expect(2 - 2).toBe(0);
-    });
-});
