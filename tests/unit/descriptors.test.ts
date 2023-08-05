8ec10d403fdbc83124be677f0a257d4515a325f7
Zero-1729 <zero1729@protonmail.com>
2023-08-05 20:31:40 +0100

    test: added descriptor unit test

---
+++ tests/unit/descriptors.test.ts
@@ -0,0 +1,39 @@
+// Tests to check that descriptors are imported and generated correctly
+import {getDescriptorParts} from './../../modules/wallet-utils';
+import {BDKWalletTypeNames} from './../../modules/wallet-defaults';
+
+describe('testing descriptor type extraction', () => {
+    test('descriptor type matches', () => {
+        const descriptor =
+            'wpkh([188ed79c/84h/1h/0h]tpubD6NzVbkrYhZ4XopgwuDUxX9FnNeZUfidCDusmRfUkzLaVKY2zNNYtqj1frtBbqTSBcHKxsbtUjD4WSDGBwiMn7mLuuWEf5WzvJKMamGNGgG/0/*)';
+        const descriptor_type = 'wpkh';
+
+        const parts = getDescriptorParts(descriptor);
+
+        expect(BDKWalletTypeNames[parts.type]).toBe(descriptor_type);
+    });
+});
+
+describe('testing descriptor checksum extraction', () => {
+    test('descriptor checksum matches', () => {
+        const descriptor =
+            "wpkh([c65d79d8/84'/0'/0']xpub6CmNYqKyLZdq1BsTyixhuNkKoa3Dt6J9pgUXjA742t7b44xAwjXZak6GvYBPda15ZqKkWippbVkCHYvHMQGuuhVsu2ohkgaVioYcNxZmEvH/0/*)#ur90lsda";
+        const descriptor_checksum = '#ur90lsda';
+
+        const parts = getDescriptorParts(descriptor);
+
+        expect(parts.checksum).toBe(descriptor_checksum);
+    });
+});
+
+describe('testing descriptor fingerprint extraction', () => {
+    test('descriptor fingerprint matches', () => {
+        const descriptor =
+            'wpkh([188ed79c/84h/1h/0h]tpubD6NzVbkrYhZ4XopgwuDUxX9FnNeZUfidCDusmRfUkzLaVKY2zNNYtqj1frtBbqTSBcHKxsbtUjD4WSDGBwiMn7mLuuWEf5WzvJKMamGNGgG/0/*)';
+        const descriptor_fingerprint = '188ed79c';
+
+        const parts = getDescriptorParts(descriptor);
+
+        expect(parts.fingerprint).toBe(descriptor_fingerprint);
+    });
+});
