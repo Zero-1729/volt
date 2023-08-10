ac771b6f680551d7ce916a147c04af34a24cbbb8
Zero-1729 <zero1729@protonmail.com>
2023-08-10 05:48:28 +0100

    test: include new descriptor parser fn

--- tests/unit/descriptors.test.ts
+++ tests/unit/descriptors.test.ts
@@ -1,16 +1,15 @@
 // Tests to check that descriptors are imported and generated correctly
-import {getDescriptorParts} from './../../modules/wallet-utils';
-import {BDKWalletTypeNames} from './../../modules/wallet-defaults';
+import {parseDescriptor} from './../../modules/descriptors';
 
 describe('testing descriptor type extraction', () => {
     test('descriptor type matches', () => {
         const descriptor =
             'wpkh([188ed79c/84h/1h/0h]tpubD6NzVbkrYhZ4XopgwuDUxX9FnNeZUfidCDusmRfUkzLaVKY2zNNYtqj1frtBbqTSBcHKxsbtUjD4WSDGBwiMn7mLuuWEf5WzvJKMamGNGgG/0/*)';
-        const descriptor_type = 'wpkh';
+        const descriptor_type = 'bech32';
 
-        const parts = getDescriptorParts(descriptor);
+        const parts = parseDescriptor(descriptor);
 
-        expect(BDKWalletTypeNames[parts.type]).toBe(descriptor_type);
+        expect(parts.type).toBe(descriptor_type);
     });
 });
 
@@ -20,7 +19,7 @@ describe('testing descriptor checksum extraction', () => {
             "wpkh([c65d79d8/84'/0'/0']xpub6CmNYqKyLZdq1BsTyixhuNkKoa3Dt6J9pgUXjA742t7b44xAwjXZak6GvYBPda15ZqKkWippbVkCHYvHMQGuuhVsu2ohkgaVioYcNxZmEvH/0/*)#ur90lsda";
         const descriptor_checksum = '#ur90lsda';
 
-        const parts = getDescriptorParts(descriptor);
+        const parts = parseDescriptor(descriptor);
 
         expect(parts.checksum).toBe(descriptor_checksum);
     });
@@ -32,7 +31,7 @@ describe('testing descriptor fingerprint extraction', () => {
             'wpkh([188ed79c/84h/1h/0h]tpubD6NzVbkrYhZ4XopgwuDUxX9FnNeZUfidCDusmRfUkzLaVKY2zNNYtqj1frtBbqTSBcHKxsbtUjD4WSDGBwiMn7mLuuWEf5WzvJKMamGNGgG/0/*)';
         const descriptor_fingerprint = '188ed79c';
 
-        const parts = getDescriptorParts(descriptor);
+        const parts = parseDescriptor(descriptor);
 
         expect(parts.fingerprint).toBe(descriptor_fingerprint);
     });
