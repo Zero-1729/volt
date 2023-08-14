e0b1b695ba0b1a6cf94e2c9ef4be769422a5d52e
Zero-1729 <zero1729@protonmail.com>
2023-08-14 23:59:20 +0100

    mod: added xprv versions list

--- modules/wallet-defaults.ts
+++ modules/wallet-defaults.ts
@@ -121,6 +121,7 @@ export const validExtendedKeyPrefixes = new Map([
 
 // Supported extended key versions
 export const xpubVersions = ['xpub', 'ypub', 'zpub', 'tpub', 'upub', 'vpub'];
+export const xprvVersions = ['xprv', 'yprv', 'zprv', 'tprv', 'uprv', 'vprv'];
 
 // Supported extended key version metadata definitions
 export const extendedKeyInfo: {[index: string]: ExtendedKeyInfoType} = {
