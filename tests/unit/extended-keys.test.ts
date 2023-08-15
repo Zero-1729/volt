94ed89ae4f1a66623efae64cc09ca17c2be8920d
Zero-1729 <zero1729@protonmail.com>
2023-08-15 01:08:17 +0100

    test: updated test to include ext key version conversions

--- tests/unit/extended-keys.test.ts
+++ tests/unit/extended-keys.test.ts
@@ -1,4 +1,8 @@
-import {getFingerprintFromXkey} from './../../modules/wallet-utils';
+import {
+    getFingerprintFromXkey,
+    convertXKey,
+    normalizeExtKey,
+} from './../../modules/wallet-utils';
 import {extendedKeyPattern} from './../../modules/re';
 
 import {Net} from '../../types/enums';
@@ -25,5 +29,30 @@ describe('testing fingerprint extraction from xpub', () => {
         const fingerprint = '188ed79c';
 
         expect(getFingerprintFromXkey(xpub, Net.Testnet)).toBe(fingerprint);
+    });
+});
+
+describe('testing extended key version conversions', () => {
+    test('extended keys match converted versions', () => {
+        const xpub =
+            'upub5EEjftjyQwjWrpRDC1LqLj3UJ3n9o3fEr912D2pTueBUjECfZFWEozahAJpmeKp44k5iftgMNoRpKKR4CJXBKrQ4CqfNCgqns87N4vWc9rq';
+        const xprv =
+            'zprvAWgYBBk7JR8Gk88z9R9VmnMDrtpkoBa9dtf6n42oSR4nmouy26Siju8bdQLf1CG7i9tQcu3RqyxzPhE99n7xgPMXaToxVbpyJEziEbX51Ur';
+
+        const expectedXpub =
+            'tpubDD7A78aQaGKQgWBR9GgoAufM95K9cJ8o979yumZGPa51dpK4PSr1pdDwTxnKAYj45Zy3XtyuHtKWfMkMkFcTbzu9sTVwdwxVGFthzgJt14k';
+        const expectedXprv =
+            'xprv9s21ZrQH143K3XkkUhaFMcADWxXruwb9ofcfDGF2gQK2fcHWWn7bVmpKazRV1NxGtseo7wrJvfFtd811iPHw5uzKqnR7KnBzknsRTS4qALz';
+
+        const convertedXpub = convertXKey(xpub, 'tpub');
+        const convertedXprv = convertXKey(xprv, 'xprv');
+
+        // Test normalization fn
+        // Only converts if exotic prefix detected
+        const normalizedXPKey = normalizeExtKey(xpub, 'pub');
+
+        expect(convertedXpub).toBe(expectedXpub);
+        expect(convertedXprv).toBe(expectedXprv);
+        expect(normalizedXPKey).toBe(convertedXpub);
     });
 });
