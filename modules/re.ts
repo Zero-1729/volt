c5f63b21c1e70d7467722e1f1e5e0507eaea3a10
Zero-1729 <zero1729@protonmail.com>
2023-08-21 10:07:47 +0100

    mod: modularized descriptor regexes

--- modules/re.ts
+++ modules/re.ts
@@ -2,26 +2,60 @@
 
 // Descriptors
 // For now, we only support single key descriptors
-// with three specific script types (legacy, P2SH, and Bech32)
-//  i.e. ‘wpkh’, ‘pkh’, ‘sh’, ‘sh(wpkh(…))’
-// Includes support for optional fingerprint + wallet origin path prefix, (i.e. [abce1234/49h/0h/0h])
-// Includes support for optional key path + wildcard suffix (i.e., /0/*)
-// Includes support for optional wallet origin path and optional wildcard (i.e., /84'/0'/0'/0/*)
-export const nativeWalletDescriptorRegex: RegExp =
-    /^((wpkh|pkh)\((\[([a-f0-9]{8})(\/[1-9]{2}(h|'))*(\/([0-9](h|')))*\])*([xyztuv]((pub|prv))[1-9A-HJ-NP-Za-km-z]{79,108})(\/[0-9]{2}(h|'))*(\/([0-9](h|')?))*(\/\*)*\))(#[a-z0-9]{8})?$/;
+// with three specific script types (i.e. ‘wpkh’, ‘pkh’, ‘sh’, ‘sh(wpkh(…))’)
+// Includes support for two kinds of inner descriptor patterns:
+// Public descriptor: [abce1234/49h/0h/0h][extkey][key path])
+// Private descriptor: [extkey][key path][key path]
+// We further enforce this in the parseDescriptor function
+const _checksumCharset: string = 'qpzry9x8gf2tvdw0s3jn54khce6mua7l';
 
-// Nested descriptors
-export const wrappedWalletDescriptorRegex: RegExp =
-    /^(sh\(wpkh\((\[([a-f0-9]{8})(\/[1-9]{2}(h|'))*(\/([0-9](h|')))*\])*([xyztuv]((pub|prv))[1-9A-HJ-NP-Za-km-z]{79,108})(\/[0-9]{2}(h|'))*(\/([0-9](h|')?))*(\/\*)*\)\))(#[a-z0-9]{8})?$/;
+// Regex patterns from from: https://github.com/bitcoin/bitcoin/blob/master/doc/descriptors.md
+// Hardened symbols: ' or h
+const hardenedRegex: string = String.raw`([h'])`;
+const wildcardRegex: string = String.raw`(\/\*)`;
+// We limit the number of digits to at least 1 and capped at 2 for now
+// Will still need to check if path valid (/44/..., /49/..., /84/...)
+// NUM or NUM'
+const numRegex: string = String.raw`([0-9]{1,2}${hardenedRegex}?)`;
+// /NUM or /NUM'
+const pathLevelRegex: string = String.raw`(\/${numRegex})`;
+// /NUM/.../* or /NUM'/.../* (follows key)
+const keyPathRegex: string = String.raw`(${pathLevelRegex}*${wildcardRegex}?)`;
+// BIP32 8 hex characters for the fingerprint of the key
+const fingerprintRegex: string = String.raw`([a-fA-F0-9]{8})`;
+// [fingerprint/NUM/]
+const keyOriginRegex: string = String.raw`(\[${fingerprintRegex}${pathLevelRegex}*\])`;
+// Trailing checksum
+const checksumRegex: string = String.raw`(#[${_checksumCharset}]{8})`;
+
+// Isolated regexes for each supported extended key
+const xprvRegex: string = String.raw`([xyztuv]prv[1-9A-HJ-NP-Za-km-z]{79,108})`;
+const xpubRegex: string = String.raw`([xyztuv]pub[1-9A-HJ-NP-Za-km-z]{79,108})`;
+
+// Extended Key Regexes
+const extendedKeyRegex: string = String.raw`(${xprvRegex}|${xpubRegex})`;
+
+// [keyOrigin]xpub1234(keyPath)
+const publicDescriptorRegex: string = String.raw`(${keyOriginRegex}+${extendedKeyRegex}${keyPathRegex}+)`;
+// xprv1234(pathLevel)(keyPath)
+const privateDescriptorRegex: string = String.raw`(${extendedKeyRegex}${pathLevelRegex}+${keyPathRegex}+)`;
+
+// Each supported regex for the supported script types
+export const wpkhDescriptorRegex: string = String.raw`(wpkh\((${publicDescriptorRegex}|${privateDescriptorRegex})\)${checksumRegex}?)`;
+export const pkhDescriptorRegex: string = String.raw`(pkh\((${publicDescriptorRegex}|${privateDescriptorRegex})\)${checksumRegex}?)`;
+export const shp2wpkhDescriptorRegex: string = String.raw`(sh\(wpkh\((${publicDescriptorRegex}|${privateDescriptorRegex})\)\)${checksumRegex}?)`;
+
+// Combined general regex pattern for all supported script types
+export const descriptorRegex: RegExp = RegExp(
+    String.raw`^(${wpkhDescriptorRegex}|${pkhDescriptorRegex}|${shp2wpkhDescriptorRegex})$`,
+);
 
 // Extended Keys
-export const extendedKeyPattern: RegExp =
-    /^([XxyYzZtuUvV](pub|prv)[1-9A-HJ-NP-Za-km-z]{79,108})$/;
-export const extendedKeyPatternG: RegExp =
-    /([XxyYzZtuUvV](pub|prv)[1-9A-HJ-NP-Za-km-z]{79,108})/g;
-export const descXpubPattern: RegExp =
-    /([xyztuv]pub[1-9A-HJ-NP-Za-km-z]{79,108})/g;
-export const xpubPattern: RegExp =
-    /^([xyztuv]pub[1-9A-HJ-NP-Za-km-z]{79,108})$/;
-export const xprvPattern: RegExp =
-    /^([xyztuv]prv[1-9A-HJ-NP-Za-km-z]{79,108})$/;
+export const extendedKeyPattern: RegExp = RegExp(
+    String.raw`^(${xpubRegex}|${xprvRegex})$`,
+);
+export const extendedKeyPatternG: RegExp = RegExp(
+    String.raw`(${xpubRegex}|${xprvRegex})g`,
+);
+export const xpubPattern: RegExp = RegExp(String.raw`^${xpubRegex}$`);
+export const xprvPattern: RegExp = RegExp(String.raw`^${xprvRegex}$`);
