3a068113d367e46744307ce6bd2aac16b04a3b45
Zero-1729 <zero1729@protonmail.com>
2023-08-25 15:26:38 +0100

    misc: minor text display updates to final send screen

--- screens/wallet/Send.tsx
+++ screens/wallet/Send.tsx
@@ -3,12 +3,11 @@ import {Text, View, useColorScheme} from 'react-native';
 
 import {SafeAreaView} from 'react-native-safe-area-context';
 
-import {CommonActions, useNavigation} from '@react-navigation/native';
+import {useNavigation, StackActions} from '@react-navigation/native';
 
 import {useTailwind} from 'tailwind-rn';
 
 import Color from '../../constants/Color';
-import Font from '../../constants/Font';
 
 import {PlainButton, LongBottomButton} from '../../components/button';
 
@@ -26,6 +25,11 @@ const SendView = ({route}: Props) => {
 
     const sats = route.params?.invoiceData?.options?.amount;
 
+    const amt =
+        sats === route.params.wallet.balance.toString()
+            ? 'max'
+            : sats + ' sats';
+
     return (
         <SafeAreaView edges={['bottom', 'left', 'right']}>
             <View
@@ -38,33 +42,38 @@ const SendView = ({route}: Props) => {
                     ]}>
                     <PlainButton
                         onPress={() =>
-                            navigation.dispatch(CommonActions.goBack())
+                            navigation.dispatch(StackActions.popToTop())
                         }
                         style={[tailwind('absolute z-10 left-6')]}>
-                        <Close fill={'white'} />
+                        <Close fill={ColorScheme.SVG.Default} />
                     </PlainButton>
-                    <Text style={[tailwind('text-sm text-white font-bold')]}>
+                    <Text
+                        style={[
+                            tailwind('text-sm font-bold'),
+                            {color: ColorScheme.Text.Default},
+                        ]}>
                         Send Transaction
                     </Text>
                 </View>
 
-                <View style={[tailwind('items-center justify-center')]}>
+                <View style={[tailwind('items-center justify-center w-full')]}>
                     <Text
                         style={[
-                            tailwind('text-base'),
+                            tailwind('text-base text-center w-4/5'),
                             {color: ColorScheme.Text.Default},
                         ]}>
-                        Transaction with {route.params?.wallet?.name} wallet
+                        Transaction with {`'${route.params?.wallet?.name}'`}{' '}
+                        wallet
                         {'\n\n'}
-                        Sending to {route.params?.invoiceData?.address}{' '}
-                        {sats ? `for ${sats} satoshis` : ''}{' '}
+                        Sending {sats ? `'${amt}'` : ''} to address:{' '}
+                        {route.params?.invoiceData?.address}{' '}
                     </Text>
                 </View>
 
                 <LongBottomButton
                     title={'Continue'}
-                    textColor={'black'}
-                    backgroundColor={'white'}
+                    textColor={ColorScheme.Text.Alt}
+                    backgroundColor={ColorScheme.Background.Inverted}
                 />
             </View>
         </SafeAreaView>
