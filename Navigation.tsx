d70184bb779f5f0360f57611ed00663dda090410
Zero-1729 <zero1729@protonmail.com>
2023-08-29 18:34:33 +0100

    nav: add feerate prop to send screen

--- Navigation.tsx
+++ Navigation.tsx
@@ -46,6 +46,7 @@ import {
     TTransaction,
     TMiniWallet,
     TAddressAmount,
+    TMempoolFeeRates,
     TInvoiceData,
 } from './types/wallet';
 
@@ -59,6 +60,7 @@ export type WalletParamList = {
     Send: {
         invoiceData: TInvoiceData;
         wallet: TMiniWallet;
+        feeRate?: number;
     };
     WalletView: undefined;
     WalletInfo: undefined;
