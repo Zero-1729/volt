1dcd3ae007d0dcf81e7ca9b3619f25b4784c8e8a
Zero-1729 <zero1729@protonmail.com>
2023-09-19 17:43:20 +0100

    misc: post merge stale changes

--- postinstall.js
+++ postinstall.js
@@ -1,10 +1,5 @@
 const exec = require('child_process').exec;
-const os = require('os');
 
 let baseCommand = `yarn build:tailwind && yarn gitbranch`;
-
-if (os.type() === 'Darwin') {
-    baseCommand += '&& react-native setup-ios-permissions';
-}
 
 exec(baseCommand);
