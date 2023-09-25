835901e789aec79361afd49f542fdd693cd1df1b
Zero-1729 <zero1729@protonmail.com>
2023-09-25 04:20:22 +0100

    build: fix ios permissions

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,10 @@
+const os = require('os');
 const exec = require('child_process').exec;
 
 let baseCommand = `yarn build:tailwind && yarn gitbranch`;
+
+if (os.type() === 'Darwin') {
+	baseCommand  += '&& react-native setup-ios-permissions';
+}
 
 exec(baseCommand);
