2e71d6bec3d1eda9ea3e8d81b32afa3082f288cc
Zero-1729 <zero1729@protonmail.com>
2024-01-20 09:55:04 +0100

    build: add universal link code for IOS
    more of a future proof inclusion

--- ios/volt/AppDelegate.m
+++ ios/volt/AppDelegate.m
@@ -32,6 +32,15 @@
 #endif
 }
 
+// Universal links
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
+
 /// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
 ///
 /// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
