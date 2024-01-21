00b048d2afb912a725165912c337f69ddc6f302d
Zero-1729 <zero1729@protonmail.com>
2024-01-22 00:46:48 +0100

    lang: added base arabic and english strings

---
+++ i18n/locales/en/index.ts
@@ -0,0 +1,11 @@
+import common from './common.json';
+import onboarding from './onboarding.json';
+import settings from './settings.json';
+import wallet from './wallet.json';
+
+export default {
+    common,
+    onboarding,
+    settings,
+    wallet,
+};
