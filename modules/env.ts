d2cbda5215bada348018119d34bc46bd51f24dbb
Zero-1729 <zero1729@protonmail.com>
2024-01-21 12:08:34 +0100

    mod: add env

---
+++ modules/env.ts
@@ -0,0 +1,5 @@
+import {BREEZ_SDK_API_KEY, BREEZ_INVITE_CODE} from '@env';
+
+// Path: modules/env.ts
+export const _BREEZ_SDK_API_KEY_ = BREEZ_SDK_API_KEY;
+export const _BREEZ_INVITE_CODE_ = BREEZ_INVITE_CODE;
