366d08001f46047e323eb27d925b4271100c7775
Zero-1729 <zero1729@protonmail.com>
2024-01-22 08:41:48 +0100

    i18n: added new translations

--- i18n/locales/en/index.ts
+++ i18n/locales/en/index.ts
@@ -2,10 +2,12 @@ import common from './common.json';
 import onboarding from './onboarding.json';
 import settings from './settings.json';
 import wallet from './wallet.json';
+import home from './home.json';
 
 export default {
     common,
     onboarding,
+    home,
     settings,
     wallet,
 };
