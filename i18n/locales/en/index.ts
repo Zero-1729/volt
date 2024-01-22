5e43c30df41c3566485217daa56d03d63d8bc761
Zero-1729 <zero1729@protonmail.com>
2024-01-22 14:53:52 +0100

    i18n: take out redundant dict

--- i18n/locales/en/index.ts
+++ i18n/locales/en/index.ts
@@ -2,12 +2,10 @@ import common from './common.json';
 import onboarding from './onboarding.json';
 import settings from './settings.json';
 import wallet from './wallet.json';
-import home from './home.json';
 
 export default {
     common,
     onboarding,
-    home,
     settings,
     wallet,
 };
