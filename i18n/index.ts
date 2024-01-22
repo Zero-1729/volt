f7e209fab53e57c1dbe05fea4588beb63ffa3170
Zero-1729 <zero1729@protonmail.com>
2024-01-22 14:50:17 +0100

    i18n: strip out fn

--- i18n/index.ts
+++ i18n/index.ts
@@ -9,59 +9,53 @@ import resources from './locales';
 export const defaultNS = 'common';
 
 const getAppLanguage = (): string => {
-    const {languageTag} = RNL.findBestLanguageTag(Object.keys(resources)) || {
-        languageTag: 'en',
-    };
-
-    return languageTag;
+    return RNL.findBestLanguageTag(Object.keys(resources))?.languageTag ?? 'en';
 };
 
 // Main instance of i18next
 // with ICU enabled plurals
-export const initI18n = async (language: string) => {
-    const i18nICU = i18n.createInstance();
+const i18nICU = i18n.createInstance();
 
-    return i18nICU
-        .use(initReactI18next)
-        .use(new ICU())
-        .init({
-            lng: language,
-            fallbackLng: getAppLanguage(),
-            resources,
-            defaultNS,
-            fallbackNS: defaultNS,
-            debug: false,
-            cache: {enabled: true},
-            interpolation: {
-                escapeValue: false,
-            },
-            returnNull: false,
-        })
-        .then(() => {
-            let timeZone = RNL.getTimeZone();
+i18nICU
+    .use(initReactI18next)
+    .use(new ICU())
+    .init({
+        lng: getAppLanguage(),
+        fallbackLng: getAppLanguage(),
+        resources,
+        defaultNS,
+        fallbackNS: defaultNS,
+        debug: false,
+        cache: {enabled: true},
+        interpolation: {
+            escapeValue: false,
+        },
+        returnNull: false,
+    })
+    .then(() => {
+        let timeZone = RNL.getTimeZone();
 
-            // if polyfill is used, we need to set default timezone
-            // https://formatjs.io/docs/polyfills/intl-datetimeformat/#default-timezone
-            if ('__setDefaultTimeZone' in Intl.DateTimeFormat) {
-                // formatjs doesn't know GMT, that is used by default in github actions
-                if (timeZone === 'GMT') {
-                    timeZone = 'Etc/GMT';
-                }
-
-                try {
-                    // @ts-ignore __setDefaultTimeZone doesn't exist in native API
-                    Intl.DateTimeFormat.__setDefaultTimeZone(timeZone);
-                } catch (e) {
-                    console.log(
-                        `error settings timezone to: ${timeZone} fallback to UTC`,
-                    );
-                    // @ts-ignore __setDefaultTimeZone doesn't exist in native API
-                    Intl.DateTimeFormat.__setDefaultTimeZone('UTC');
-                    timeZone = 'UTC';
-                }
+        // if polyfill is used, we need to set default timezone
+        // https://formatjs.io/docs/polyfills/intl-datetimeformat/#default-timezone
+        if ('__setDefaultTimeZone' in Intl.DateTimeFormat) {
+            // formatjs doesn't know GMT, that is used by default in github actions
+            if (timeZone === 'GMT') {
+                timeZone = 'Etc/GMT';
             }
-        });
-};
+
+            try {
+                // @ts-ignore __setDefaultTimeZone doesn't exist in native API
+                Intl.DateTimeFormat.__setDefaultTimeZone(timeZone);
+            } catch (e) {
+                console.log(
+                    `error settings timezone to: ${timeZone} fallback to UTC`,
+                );
+                // @ts-ignore __setDefaultTimeZone doesn't exist in native API
+                Intl.DateTimeFormat.__setDefaultTimeZone('UTC');
+                timeZone = 'UTC';
+            }
+        }
+    });
 
 export default i18n;
 
