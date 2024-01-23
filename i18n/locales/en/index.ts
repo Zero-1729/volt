639b6f9327dfc965a4ff7c96e55941a5b9659c62
Zero-1729 <zero1729@protonmail.com>
2024-01-23 01:01:36 +0100

    i18n: added errors and warn namespace

--- i18n/locales/en/index.ts
+++ i18n/locales/en/index.ts
@@ -2,10 +2,12 @@ import common from './common.json';
 import onboarding from './onboarding.json';
 import settings from './settings.json';
 import wallet from './wallet.json';
+import errors from './errors.json';
 
 export default {
     common,
     onboarding,
     settings,
     wallet,
+    errors,
 };
