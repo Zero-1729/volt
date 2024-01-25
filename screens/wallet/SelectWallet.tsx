64791bc71b87338729e4570b081dcc9f2818c00a
Zero-1729 <zero1729@protonmail.com>
2024-01-25 13:02:11 +0100

    screen: display invoice data in select wallet screen

--- screens/wallet/SelectWallet.tsx
+++ screens/wallet/SelectWallet.tsx
@@ -1,6 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useContext, useEffect} from 'react';
-import {Text, View, useColorScheme, Platform, Dimensions} from 'react-native';
+import {
+    Text,
+    View,
+    useColorScheme,
+    Platform,
+    Dimensions,
+    StyleSheet,
+} from 'react-native';
+
+import VText from '../../components/text';
 
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {InitStackParamList} from '../../Navigation';
@@ -23,12 +32,17 @@ import {useTailwind} from 'tailwind-rn';
 
 import Color from '../../constants/Color';
 
+import BigNumber from 'bignumber.js';
+
 import {LongBottomButton} from '../../components/button';
+import {FiatBalance, DisplaySatsAmount} from '../../components/balance';
 
 import {WalletCard} from '../../components/card';
 import {BaseWallet} from '../../class/wallet/base';
 import {TInvoiceData} from '../../types/wallet';
 import {useNetInfo} from '@react-native-community/netinfo';
+
+import InfoIcon from '../../assets/svg/info-16.svg';
 
 type Props = NativeStackScreenProps<InitStackParamList, 'SelectWallet'>;
 
@@ -37,8 +51,9 @@ const SelectWallet = ({route}: Props) => {
 
     const tailwind = useTailwind();
 
-    const {t} = useTranslation('wallet');
+    const {t, i18n} = useTranslation('wallet');
     const {t: e} = useTranslation('errors');
+    const langDir = i18n.dir() === 'rtl' ? 'right' : 'left';
 
     const navigation = useNavigation();
     const [decodedInvoice, setDecodedInvoice] = useState<TInvoiceData>(
@@ -56,7 +71,7 @@ const SelectWallet = ({route}: Props) => {
 
     const AppScreenWidth = Dimensions.get('window').width;
 
-    const cardHeight = 230;
+    const cardHeight = 220;
 
     const decodeInvoice = (invoice: string) => {
         // TODO: handle decoding Lightning invoices
@@ -192,6 +207,10 @@ const SelectWallet = ({route}: Props) => {
         );
     };
 
+    const sats = decodedInvoice.options?.amount
+        ? decodedInvoice.options?.amount * 100_000_000
+        : undefined;
+
     return (
         <SafeAreaView>
             <View
@@ -204,53 +223,195 @@ const SelectWallet = ({route}: Props) => {
                 <View
                     style={[
                         tailwind('w-5/6 items-center flex-row justify-between'),
-                        {marginTop: topPlatformOffset + 12},
+                        {marginTop: topPlatformOffset},
                     ]}>
                     <Text
                         style={[
                             tailwind('text-center font-bold text-xl w-full'),
                             {color: ColorScheme.Text.Default},
                         ]}>
-                        Select Wallet
+                        {t('select_wallet_title')}
                     </Text>
                 </View>
 
-                <View style={[tailwind('justify-center mt-12')]}>
+                {/*Display the invoice data */}
+                <View
+                    style={[
+                        tailwind(
+                            'w-5/6 p-6 items-center flex justify-between rounded mt-4',
+                        ),
+                    ]}>
+                    {decodedInvoice.options?.label && (
+                        <Text
+                            numberOfLines={1}
+                            ellipsizeMode="middle"
+                            style={[
+                                tailwind(
+                                    'w-full text-center font-bold text-lg mb-2',
+                                ),
+                                {
+                                    color: ColorScheme.Text.Default,
+                                },
+                            ]}>
+                            {decodedInvoice.options.label}
+                        </Text>
+                    )}
+                    {decodedInvoice.options?.amount && sats && (
+                        <View
+                            style={[tailwind('w-full items-center flex mb-2')]}>
+                            <FiatBalance
+                                balance={sats}
+                                loading={false}
+                                balanceFontSize={'text-4xl'}
+                                fontColor={ColorScheme.Text.Default}
+                                ignoreHideBalance={true}
+                            />
+                            <View
+                                style={[
+                                    tailwind('w-full mt-4'),
+                                    styles.invoiceLineBreaker,
+                                    {
+                                        borderColor:
+                                            ColorScheme.Background.Inverted,
+                                    },
+                                ]}
+                            />
+                            <View
+                                style={[
+                                    tailwind(
+                                        'w-full items-center flex-row justify-between mt-4',
+                                    ),
+                                ]}>
+                                <VText
+                                    style={[
+                                        tailwind('font-bold'),
+                                        {color: ColorScheme.Text.Default},
+                                    ]}>
+                                    Amount
+                                </VText>
+                                <DisplaySatsAmount
+                                    amount={new BigNumber(sats)}
+                                    fontSize="text-sm"
+                                    isApprox={false}
+                                    textColor={ColorScheme.Text.GrayText}
+                                />
+                            </View>
+                        </View>
+                    )}
+
+                    <View
+                        style={[
+                            tailwind(
+                                'w-full items-center flex justify-between mb-4',
+                            ),
+                        ]}>
+                        <VText
+                            style={[
+                                tailwind('font-bold w-full mb-2'),
+                                {color: ColorScheme.Text.Default},
+                            ]}>
+                            Address
+                        </VText>
+                        <VText
+                            style={[
+                                tailwind('w-full'),
+                                {color: ColorScheme.Text.GrayText},
+                            ]}>
+                            {decodedInvoice.address}
+                        </VText>
+                    </View>
+
+                    {/* Display the message */}
+                    {decodedInvoice.options?.message && (
+                        <>
+                            <View
+                                style={[
+                                    tailwind('w-full mb-6 opacity-20'),
+                                    styles.invoiceLineBreaker,
+                                    {
+                                        borderColor:
+                                            ColorScheme.Background.Inverted,
+                                    },
+                                ]}
+                            />
+
+                            <Text
+                                numberOfLines={2}
+                                ellipsizeMode="middle"
+                                style={[
+                                    tailwind('font-bold'),
+                                    {color: ColorScheme.Text.GrayText},
+                                ]}>
+                                {decodedInvoice.options.message}
+                            </Text>
+                        </>
+                    )}
+                </View>
+
+                <View style={[tailwind('w-5/6 justify-center mt-2')]}>
                     {/** Carousel for 'BaseCard */}
-                    {wallets.length > 0 ? (
+                    <View
+                        style={[tailwind('self-center'), {height: cardHeight}]}>
+                        <Carousel
+                            enabled={
+                                walletMode === 'multi' && wallets.length > 1
+                            }
+                            vertical={true}
+                            autoPlay={false}
+                            width={AppScreenWidth * 0.9}
+                            height={cardHeight}
+                            data={
+                                walletMode === 'single'
+                                    ? [wallets[walletsIndex]]
+                                    : [...wallets]
+                            }
+                            renderItem={renderCard}
+                            pagingEnabled={true}
+                            mode={'vertical-stack'}
+                            modeConfig={{
+                                snapDirection: 'left',
+                                stackInterval: 8,
+                            }}
+                            onScrollEnd={index => {
+                                updateWalletId(wallets[index].id);
+                            }}
+                            defaultIndex={walletsIndex}
+                        />
+                    </View>
+
+                    {walletMode === 'multi' && (
                         <View
                             style={[
-                                tailwind('self-center'),
-                                {height: cardHeight},
+                                tailwind(
+                                    `w-full ${
+                                        langDir === 'right'
+                                            ? 'flex-row-reverse'
+                                            : 'flex-row'
+                                    } justify-center items-center`,
+                                ),
                             ]}>
-                            <Carousel
-                                enabled={
-                                    walletMode === 'multi' && wallets.length > 1
-                                }
-                                vertical={true}
-                                autoPlay={false}
-                                width={AppScreenWidth * 0.92}
-                                height={cardHeight}
-                                data={
-                                    walletMode === 'single'
-                                        ? [wallets[walletsIndex]]
-                                        : [...wallets]
-                                }
-                                renderItem={renderCard}
-                                pagingEnabled={true}
-                                mode={'vertical-stack'}
-                                modeConfig={{
-                                    snapDirection: 'left',
-                                    stackInterval: 8,
-                                }}
-                                onScrollEnd={index => {
-                                    updateWalletId(wallets[index].id);
-                                }}
-                                defaultIndex={walletsIndex}
+                            <InfoIcon
+                                style={[
+                                    tailwind(
+                                        `${
+                                            langDir === 'right'
+                                                ? 'ml-2'
+                                                : 'mr-2'
+                                        }`,
+                                    ),
+                                ]}
+                                width={14}
+                                height={14}
+                                fill={ColorScheme.SVG.GrayFill}
                             />
+                            <Text
+                                style={[
+                                    tailwind('text-sm'),
+                                    {color: ColorScheme.Text.DescText},
+                                ]}>
+                                {t('select_wallet_swipe_notice')}
+                            </Text>
                         </View>
-                    ) : (
-                        <></>
                     )}
                 </View>
 
@@ -266,3 +427,10 @@ const SelectWallet = ({route}: Props) => {
 };
 
 export default SelectWallet;
+
+const styles = StyleSheet.create({
+    invoiceLineBreaker: {
+        borderBottomWidth: 1,
+        opacity: 0.1,
+    },
+});
