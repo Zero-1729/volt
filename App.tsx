158b3196dbc7fa05ca3ea878b37aa83762c33223
Zero-1729 <zero1729@protonmail.com>
2024-01-25 09:19:27 +0100

    app: added trigger checks for deep link and clipboard
    - We check for deep link and clipboard if newly launched.
    - We check for clipboard contents each time app moves into foreground

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import 'react-native-gesture-handler';
 
-import React, {useContext, useEffect} from 'react';
-import {StatusBar, useColorScheme, Linking} from 'react-native';
+import React, {useContext, useEffect, useRef} from 'react';
+import {StatusBar, useColorScheme, Linking, AppState} from 'react-native';
+
+import {useRenderCount} from './modules/hooks';
 
 import {AppStorageContext} from './class/storageContext';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -29,6 +31,9 @@ import Color from './constants/Color';
 import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+    const appState = useRef(AppState.currentState);
+    const renderCount = useRenderCount();
+
     const {appLanguage} = useContext(AppStorageContext);
     const {t} = useTranslation('wallet');
 
@@ -106,14 +111,53 @@ const App = () => {
         },
     };
 
+    // Check deep link & clipboard if app newly launched if app previously unopened
+    const checkDeepLinkAndClipboard = async (): Promise<void> => {
+        // Check deep link
+        const url = await Linking.getInitialURL();
+
+        if (url) {
+            rootNavigation.navigate('SelectWallet', {invoice: url});
+            return;
+        }
+
+        // Check clipboard
+        checkAndSetClipboard();
+    };
+
     useEffect(() => {
         SplashScreen.hide();
 
         // Enable privacy blur for IOS; blur screen when screen inactive
         Privacy?.enabled(true);
 
-        // Check clipboard contents
-        checkAndSetClipboard();
+        // Check for deep link if app newly launched
+        if (renderCount <= 2) {
+            checkDeepLinkAndClipboard();
+        }
+
+        // TODO: Add check for Auth when app is active
+        const appStateSub = AppState.addEventListener(
+            'change',
+            (incomingState): void => {
+                // Check and run clipboard fn if app is active in foreground
+                if (
+                    appState.current.match(/background/) &&
+                    incomingState === 'active'
+                ) {
+                    // Check clipboard contents
+                    checkAndSetClipboard();
+                }
+
+                // Update app state
+                appState.current = incomingState;
+            },
+        );
+
+        return () => {
+            // Kill subscription
+            appStateSub.remove();
+        };
     }, []);
 
     useEffect(() => {
