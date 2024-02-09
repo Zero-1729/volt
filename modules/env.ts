64b1f8717fd208e80fe0f442cd44ca798a0a4f4f
Zero-1729 <zero1729@protonmail.com>
2024-02-09 06:42:34 +0100

    mod: minor switch

--- modules/env.ts
+++ modules/env.ts
@@ -1,5 +1,5 @@
-import {BREEZ_SDK_API_KEY, BREEZ_INVITE_CODE} from '@env';
+import {BREEZ_INVITE_CODE, BREEZ_SDK_API_KEY} from '@env';
 
 // Path: modules/env.ts
-export const _BREEZ_SDK_API_KEY_ = BREEZ_SDK_API_KEY;
 export const _BREEZ_INVITE_CODE_ = BREEZ_INVITE_CODE;
+export const _BREEZ_SDK_API_KEY_ = BREEZ_SDK_API_KEY;
