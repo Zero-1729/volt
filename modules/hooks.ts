e0a79f0f4d46d61152c43c9f4073237d008f380e
Zero-1729 <zero1729@protonmail.com>
2024-02-11 14:14:03 +0100

    mod: added countdown custom hook

--- modules/hooks.ts
+++ modules/hooks.ts
@@ -1,5 +1,5 @@
 // helper for React hooks
-import {useRef} from 'react';
+import {useRef, useEffect, useState} from 'react';
 
 // Render count helper
 export const useRenderCount = (): number => {
@@ -7,4 +7,36 @@ export const useRenderCount = (): number => {
     renderCount.current += 1;
 
     return renderCount.current;
+};
+
+// Countdown helper
+// countdown value handler
+const _getTimeValues = (sec: number) => {
+    // calculate time left
+    const days = Math.floor(sec / (60 * 60 * 24));
+    const hours = Math.floor((sec % (60 * 60 * 24)) / (60 * 60));
+    const minutes = Math.floor(sec / 60);
+    const seconds = Math.floor(sec - minutes * 60);
+
+    return [days, hours, minutes, seconds];
+};
+
+export const useCountdown = (sec: number) => {
+    // Initial value of timer
+    const [timestamp, setTimestamp] = useState(sec);
+
+    useEffect(() => {
+        if (timestamp <= 0) {
+            return;
+        }
+
+        // Update the timer every second
+        const interval = setInterval(() => {
+            setTimestamp(t => t - 1);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [timestamp]);
+
+    return _getTimeValues(timestamp);
 };
