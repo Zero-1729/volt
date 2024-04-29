674e0141f5d30113a30ae1ebe9b37ed6ac88e80e
Zero-1729 <zero1729@protonmail.com>
2024-04-29 13:38:10 +0100

    comp: add pin attempts and forgot PIN to PIN component

--- components/pinpass.tsx
+++ components/pinpass.tsx
@@ -1,13 +1,18 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, {useMemo, useEffect, useState} from 'react';
+import React, {useMemo, useEffect, useState, useContext} from 'react';
 import {View, Text, useColorScheme, StyleSheet} from 'react-native';
 
 import {BottomSheetModal} from '@gorhom/bottom-sheet';
 import {BottomModal} from './bmodal';
 import Color from '../constants/Color';
 
+import {PlainButton} from './button';
+
 import {useTailwind} from 'tailwind-rn';
 import {useTranslation} from 'react-i18next';
+
+import {CommonActions, useNavigation} from '@react-navigation/native';
 
 import {PinNumpad} from './input';
 
@@ -16,6 +21,10 @@ import {getKeychainItem} from '../class/keychainContext';
 import Toast, {ToastConfig} from 'react-native-toast-message';
 import {toastConfig} from './toast';
 import NativeWindowMetrics from '../constants/NativeWindowMetrics';
+
+import {MAX_PIN_ATTEMPTS} from '../modules/wallet-defaults';
+
+import {AppStorageContext} from '../class/storageContext';
 
 type PinPassProps = {
     pinPassRef: React.RefObject<BottomSheetModal>;
@@ -27,8 +36,12 @@ type PinPassProps = {
 const PinPass = (props: PinPassProps) => {
     const tailwind = useTailwind();
     const ColorScheme = Color(useColorScheme());
+    const navigation = useNavigation();
 
-    const snapPoints = useMemo(() => ['70'], []);
+    const {setPINAttempts, pinAttempts, resetAppData} =
+        useContext(AppStorageContext);
+
+    const snapPoints = useMemo(() => ['75'], []);
 
     const [tmpPIN, setTmpPIN] = useState<string>('');
     const [validPin, setValidPin] = useState<string>('');
@@ -39,14 +52,31 @@ const PinPass = (props: PinPassProps) => {
         setTmpPIN(pin);
     };
 
+    const routeToResetPIN = () => {
+        navigation.dispatch(
+            CommonActions.navigate('SettingsRoot', {
+                screen: 'ResetPIN',
+                params: {isPINReset: true},
+            }),
+        );
+    };
+
     useEffect(() => {
         if (tmpPIN.length === 4) {
+            if (pinAttempts === MAX_PIN_ATTEMPTS) {
+                // WARNING: Reset wallet data
+                resetAppData();
+            }
+
             if (tmpPIN === validPin) {
                 props.triggerSuccess();
                 setTmpPIN('');
+                setPINAttempts(0);
+                return;
             }
 
             setTmpPIN('');
+            setPINAttempts(pinAttempts + 1);
         }
     }, [tmpPIN]);
 
@@ -80,14 +110,73 @@ const PinPass = (props: PinPassProps) => {
                 <View style={[tailwind('w-full px-2 h-full items-center')]}>
                     <Text
                         style={[
-                            tailwind('text-center text-lg mt-4'),
+                            tailwind('text-center text-lg font-bold mt-4'),
                             {color: ColorScheme.Text.Default},
                         ]}>
                         {t('lock_screen_message')}
                     </Text>
 
+                    {pinAttempts > 0 && (
+                        <>
+                            <View
+                                style={[
+                                    tailwind('items-center mt-6 mb-4 w-5/6'),
+                                ]}>
+                                {pinAttempts === MAX_PIN_ATTEMPTS - 1 ? (
+                                    <Text
+                                        style={[
+                                            tailwind('text-sm text-center'),
+                                            {
+                                                color: ColorScheme.Text.Default,
+                                            },
+                                        ]}>
+                                        {t('last_attempt_warning')}
+                                    </Text>
+                                ) : (
+                                    <Text
+                                        style={[
+                                            tailwind('text-sm'),
+                                            {
+                                                color: ColorScheme.Text.Default,
+                                            },
+                                        ]}>
+                                        {t('pin_attempts', {
+                                            attempts:
+                                                MAX_PIN_ATTEMPTS - pinAttempts,
+                                        })}
+                                    </Text>
+                                )}
+                            </View>
+
+                            <PlainButton onPress={routeToResetPIN}>
+                                <View
+                                    style={[
+                                        tailwind('rounded-full px-4 py-1 mb-8'),
+                                        {
+                                            backgroundColor:
+                                                ColorScheme.Background.Greyed,
+                                        },
+                                    ]}>
+                                    <Text
+                                        style={[
+                                            tailwind('text-sm'),
+                                            {
+                                                color: ColorScheme.Text
+                                                    .DescText,
+                                            },
+                                        ]}>
+                                        {t('forgot_pin')}
+                                    </Text>
+                                </View>
+                            </PlainButton>
+                        </>
+                    )}
+
                     <View
-                        style={[tailwind('flex-row items-center mt-12 mb-4')]}>
+                        style={[
+                            tailwind('flex-row items-center'),
+                            {marginTop: pinAttempts > 0 ? 0 : 98},
+                        ]}>
                         {Array(4)
                             .fill(null)
                             .map((_, i) => (
