f0fc7f3b2987246faaa5052db2d6c09f5db6644c
Zero-1729 <zero1729@protonmail.com>
2024-05-17 14:02:58 +0100

    build: take out Flipper

--- react-native.config.js
+++ react-native.config.js
@@ -1,8 +1,4 @@
 module.exports = {
     assets: ['./assets/fonts/'],
-    dependencies: {
-        ...(process.env.NO_FLIPPER
-            ? {'react-native-flipper': {platforms: {ios: null}}}
-            : {}),
-    },
+    dependencies: {},
 };
