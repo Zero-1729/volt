082d3a94c84478dac7e5a812beb5aa577135be11
Zero-1729 <zero1729@protonmail.com>
2024-05-19 23:46:51 +0100

    mod: added NFC parsing module

---
+++ modules/nfc.ts
@@ -0,0 +1,31 @@
+import {TagEvent} from 'react-native-nfc-manager';
+
+// Return digestible NFC tag data or error
+export const extractNFCTagData = (nfcTag: TagEvent) => {
+    // Validate NFC tag data
+    if (!nfcTag) {
+        throw new Error('Invalid NFC tag data');
+    }
+
+    const payload = nfcTag.ndefMessage[0].payload;
+    const rawBytesText = Buffer.from(payload).toString('utf-8');
+    const rawLnurl =
+        rawBytesText[0] === '\x00' ? rawBytesText.slice(1) : rawBytesText;
+    const lnurl =
+        rawLnurl.startsWith('lnurlw://') || rawLnurl.startsWith('lnurl')
+            ? rawLnurl
+            : null;
+
+    // Extract NFC tag data
+    const nfcTagData = {
+        tagId: nfcTag.id, // 14-digit
+        tagType: nfcTag.type, // NFC Forum Type 4 (on Android)
+        tagTechs: nfcTag.techTypes, // array including 'android.nfc.tech.Ndef'
+        lnurl: lnurl, // null or lnurlw://...
+        size: nfcTag.maxSize, // in bytes, e.g. 256
+        rawTagMessage: nfcTag.ndefMessage, // array of NDEF records
+    };
+
+    // Return NFC tag data
+    return nfcTagData;
+};
