3dd612cada032fdf58bae675802da53ef27b9a9a
Zero-1729 <zero1729@protonmail.com>
2024-05-30 08:07:12 +0100

    screen: use i18n translation for loading text

--- screens/settings/Currency.tsx
+++ screens/settings/Currency.tsx
@@ -266,7 +266,7 @@ const Currency = () => {
                                                     .GrayedText,
                                             },
                                         ]}>
-                                        Loading rate...
+                                        {t('loading_rate')}
                                     </VText>
                                 </View>
                             ) : (
