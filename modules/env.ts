1ba55814dfac753d19781fb612b2ed92a6a06234
Zero-1729 <zero1729@protonmail.com>
2024-06-06 12:33:04 +0200

    mod: export Breez SDK credentials from env

--- modules/env.ts
+++ modules/env.ts
@@ -1,5 +1,12 @@
-import {BREEZ_INVITE_CODE, BREEZ_SDK_API_KEY} from '@env';
+import {
+    BREEZ_INVITE_CODE,
+    BREEZ_SDK_API_KEY,
+    GL_CUSTOM_NOBODY_CERT,
+    GL_CUSTOM_NOBODY_KEY,
+} from '@env';
 
 // Path: modules/env.ts
 export const _BREEZ_INVITE_CODE_ = BREEZ_INVITE_CODE;
 export const _BREEZ_SDK_API_KEY_ = BREEZ_SDK_API_KEY;
+export const _GL_CUSTOM_NOBODY_CERT_ = GL_CUSTOM_NOBODY_CERT;
+export const _GL_CUSTOM_NOBODY_KEY_ = GL_CUSTOM_NOBODY_KEY;
