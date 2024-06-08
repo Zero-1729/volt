8105ea72dee6e44084aa81577af0f2fb89fd823d
Zero-1729 <zero1729@protonmail.com>
2024-06-08 17:22:44 +0100

    ts: included GL Key and Cert type for env

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,4 +1,6 @@
 declare module '@env' {
     export const BREEZ_INVITE_CODE: string;
     export const BREEZ_SDK_API_KEY: string;
+    export const GL_CUSTOM_NOBODY_KEY: string;
+    export const GL_CUSTOM_NOBODY_CERT: string;
 }
